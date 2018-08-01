c9184e4cae12b3dd7716916119399a5c421b7ac2
Arun <arunva28@gmail.com>
2018-08-01 11:42:36 -0400

    expense income added
    accounts/views.py
    accounts/urls.py
    accounts/serializers.py
    accounts/models.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import AccountsModel
+from .models import AccountsModel, Description
 
 
 class AccountsSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountsModel
         fields = '__all__'
+
+
+class AccountsDescriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Description
+        fields = '__all__'
+
+
+class AccountsExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountsModel
+        fields = ('Amount',)
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
      #url('^accounts/', login_required(views.AccountsView.as_view())),
      url('^accounts/', login_required((views.AccountsView.as_view()))),
+     url('^accounts_Dropdown/', login_required((views.AccountsDropdownview.as_view()))),
+     url('analyze/', login_required((views.Expenses.as_view())))
         ]
 
